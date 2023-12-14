404c4a8822e6d5e7cb0c3ed759415b763afa5e85
DevDeshmukh <deshmukhvaishnavi1309@gmail.com>
2023-12-15 00:25:51 +0530

    Pilot File

---
+++ Main.py
@@ -0,0 +1,17 @@
+# Way 1:
+
+# # numbers = [int(num) for num in input("enter the no:").split()]
+# sum = sum(numbers)
+# print(f"the sum of numbers is:{sum}")
+# Way 2:
+
+# a = input("enter no")
+# sum =0
+# for i in a:
+#     sum = sum + int(i)
+# print(sum)
+a = input("enter numbers")
+sum =0
+for i in a:
+    sum = sum+int(i)
+print(sum)
